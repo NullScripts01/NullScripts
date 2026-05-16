a27a4ae0bf646dc40919e44cd1d34dedc0eb04f4
NullS <52143116+NullScripts01@users.noreply.github.com>
2026-05-15 23:22:36 -0300

    feat: update script for Script 5555555

---
+++ Scripts/script-5555555(5555555)/5555555.lua
@@ -0,0 +1,5 @@
+Barrinha inferior que de pra arrastar da ui
+"Corner bar" que de pra aumentar a ui ou diminuir (com limites para não bugar os elementos internos etc)
+Descripition em todos os elementos.
+Icon em todos so elementos
+Locked Element e muito mais kkkkk idk
